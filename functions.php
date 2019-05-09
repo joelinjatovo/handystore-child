305cfa1cf6b035ac5c59000e4bd30ac7d4c77f4d
Joelinjatovo <joelinjatovo@gmail.com>
2019-05-09 18:48:07 +0300

    Boite de message

--- functions.php
+++ functions.php
@@ -332,7 +332,7 @@ function passion_vendor_box_shortcode($atts){
         }
         
         echo '<div class="vendor-message-seller-container">';
-          echo '<a class="button" id="po-message-seller" href="#" rel="nofollow">';
+          echo '<a class="button po-message-seller-right" id="po-message-seller" href="#" rel="nofollow">';
                 echo '<i class="fa fa-envelope-o" aria-hidden="true"></i>Contacter le vendeur';
             echo '</a>';
         echo '</div>';
@@ -355,7 +355,7 @@ function passion_vendor_box_shortcode($atts){
             });
 
             // Show the login/signup popup on click
-            $('#po-message-seller').on('click', function (e) {
+            $('.po-message-seller-right').on('click', function (e) {
                 var overlay = '<div class="messenger_overlay"></div>';
                 $('body').prepend($(overlay).css('opacity', '0.5'));
                 $('#po-vendor-message-seller').fadeIn(300);
